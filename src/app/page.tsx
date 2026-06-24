dc7c8828be463842a85b7f72c92673e41a2881cf
aLMASIK <suleimenovalmas75@gmail.com>
2026-06-24 23:59:43 +0500

    dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,52 @@
-import { About } from "@/components/About/About";
-import { Advantages } from "@/components/Advantages/Advantages";
-import { CinematicTour } from "@/components/CinematicTour/CinematicTour";
-import { Cta } from "@/components/Cta/Cta";
-import { FloorPlan } from "@/components/FloorPlan/FloorPlan";
-import { Footer } from "@/components/Footer/Footer";
-import { HorizontalGallery } from "@/components/Gallery/HorizontalGallery";
-import { Hero } from "@/components/Hero/Hero";
+import dynamic from "next/dynamic";
 import { Navbar } from "@/components/Navbar/Navbar";
-import { Location } from "@/components/Location/Location";
+import { Hero } from "@/components/Hero/Hero";
+import { About } from "@/components/About/About";
+import { Footer } from "@/components/Footer/Footer";
+
+// Асинхронная подгрузка тяжелых компонентов (без запрещенного флага)
+const CinematicTour = dynamic(() => 
+  import("@/components/CinematicTour/CinematicTour").then((mod) => mod.CinematicTour)
+);
+
+const FloorPlan = dynamic(() => 
+  import("@/components/FloorPlan/FloorPlan").then((mod) => mod.FloorPlan)
+);
+
+const Advantages = dynamic(() => 
+  import("@/components/Advantages/Advantages").then((mod) => mod.Advantages)
+);
+
+const HorizontalGallery = dynamic(() => 
+  import("@/components/Gallery/HorizontalGallery").then((mod) => mod.HorizontalGallery)
+);
+
+const Location = dynamic(() => 
+  import("@/components/Location/Location").then((mod) => mod.Location)
+);
+
+const Cta = dynamic(() => 
+  import("@/components/Cta/Cta").then((mod) => mod.Cta)
+);
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      
+      {/* Критически важный контент */}
       <Hero />
       <About />
       
-
+      {/* Ленивая загрузка по мере скролла */}
       <CinematicTour />
       <FloorPlan />
       <Advantages />
       <HorizontalGallery />
-      <Cta/>
-      <Location/>
+      <Cta />
+      <Location />
       
-
-      <Footer/>
-      
+      <Footer />
     </main>
   );
 }
